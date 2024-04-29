63bc891f91eef63419cfbfd1ab9ea40c4ece5353
Steffen T <steffmturner94@gmail.com>
2024-04-29 18:55:23 -0500

    HOUR 2

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Input from "./compnonents/Input";
+import Upload from "./compnonents/UploadFile";
+import Submit from "./compnonents/SubmitButton";
+function App() {
+  return (
+    <main>
+      <Input />
+      <Upload />
+      <Submit />
+    </main>
+  );
+}
+
+export default App;
