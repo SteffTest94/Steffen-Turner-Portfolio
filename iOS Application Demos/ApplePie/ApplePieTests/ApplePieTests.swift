47b60ecbc175f69a19e1d7260eef4ddfa70f5b01
Your Name <you@example.com>
2024-12-22 13:53:03 -0600

    Added a section for iOS Application Demos; this is for applications made with Swift, in XCode, while going through Apple's learning materials for Swift

---
+++ iOS Application Demos/ApplePie/ApplePieTests/ApplePieTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePieTests.swift
+//  ApplePieTests
+//
+//  Created by Steffen Turner on 11/15/24.
+//
+
+import Testing
+@testable import ApplePie
+
+struct ApplePieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
